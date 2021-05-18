d980f47cbb0923f7b6c0105b722179cdcd0d82eb
Gustavo Henrique <64941114+GustavHenrique@users.noreply.github.com>
2021-05-18 00:03:33 -0300

    initial commit

---
+++ src/main/java/teste/array/App.java
@@ -0,0 +1,20 @@
+package teste.array;
+import java.util.Scanner;
+
+class App {
+    public static void main(String[] args) {
+
+    String[] arrayString = new String[25];
+    Scanner myObj = new Scanner(System.in);
+    int i;
+
+        for (i = 0; i < arrayString.length; i++) {
+            System.out.println("Enter username");
+            arrayString[i] = myObj.nextLine();
+            System.out.println(i + "Username ["+i+"]: " + arrayString[i]);
+        }
+        
+    }
+
+}
+

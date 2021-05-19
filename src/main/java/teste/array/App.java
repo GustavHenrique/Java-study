310587abdb00bd1a861a451b5a8029d2e997b315
Gustavo Henrique <64941114+GustavHenrique@users.noreply.github.com>
2021-05-19 01:31:05 -0300

    release: 1.0.0

--- src/main/java/teste/array/App.java
+++ src/main/java/teste/array/App.java
@@ -11,17 +11,62 @@ class App {
         try {
             
             for (i = 0; i < arrayString.length; i++) {
-                System.out.println("Enter username");         
+
+                System.out.println("Digite o nome do aluno " +(i+1)+":");         
                 arrayString[i] = myObj.nextLine();
-                System.out.println("Username ["+i+"]: " + arrayString[i]);
+                System.out.println("Aluno ["+(i+1)+"]: " + arrayString[i]);
+
             }
+
+            System.out.println("");
+            System.out.println("Alunos cadastrados:");
+            System.out.println("");
+
+            for (i = 0; i < arrayString.length; i++) {
+
+                System.out.println("Aluno ["+(i+1)+"]: " + arrayString[i]);
+
+            }
+
+            System.out.println("");
+            System.out.println("Cadastre mais 5 alunos:");
+            System.out.println("");
             
+            String arrayString2[] = prepareArray(arrayString, 5);
+
+            for (i = arrayString.length; i < arrayString2.length; i++) {
+
+                System.out.println("Digite o nome do aluno " +(i+1)+":");         
+                arrayString2[i] = myObj.nextLine();
+                System.out.println("Aluno ["+(i+1)+"]: " + arrayString2[i]);
+
+            }
+
+            System.out.println("");
+            System.out.println("Alunos cadastrados:");
+            System.out.println("");
+
+            for (i = 0; i < arrayString2.length; i++) {
+
+                System.out.println("Aluno ["+(i+1)+"]: " + arrayString2[i]);
+
+            }
+
         } finally {
 
             myObj.close();
 
         }
         
+    } 
+
+    public static String[] prepareArray(String[] array, int i) {
+        
+        String arrayString2[] = new String[array.length + i];
+
+        System.arraycopy(array, 0, arrayString2, 0, array.length);
+
+        return arrayString2;
     }
 
 }
